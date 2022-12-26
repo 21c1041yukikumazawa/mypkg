5788d3caed3e659f4db9eae78a01684bc8d59a77
Yuki Kumazawa <s21c1041nr@s.chibakoudai.jp>
2022-12-27 08:12:49 +0900

    test.bash

---
+++ test.bash
@@ -0,0 +1,12 @@
+PDX-FileCopyrightText: 2022 Kumazawa Yuki
+# SPDX-License-Identifier: BSD-3-Clause　
+ng () {
+      echo NG at Line $1
+      res=1
+}
+
+res=0
+out=$(seq 5 | ./plusmult)
+[ "${out}" = '15 120' ] || ng ${LINENO}
+[ "$res" = 0 ] && echo OK        
+exit $res
